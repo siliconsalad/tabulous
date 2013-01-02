50c37fa37c4fde20b0cb663c1884313bba3c9e07
Kevin Disneur <kevin.disneur+github@gmail.com>
2013-01-02 17:14:00 +0100

    ass helper to check if current instance has subtabs

--- lib/tabulous/helpers.rb
+++ lib/tabulous/helpers.rb
@@ -1,6 +1,13 @@
 module Tabulous
   module Helpers
-    
+
+    def has_subtabs?
+      if (tab = Tabulous.active_tab(self))
+        return tab.subtabs.present?
+      end
+      false
+    end
+
     def tabs
       Tabulous.render_tabs(self)
     end
